7204e33624812952744445e97fd487fc7c64c34b
thiago <hollebenthiago@gmail.com>
2020-06-02 00:40:04 -0300

    cleaning up drawer script

--- js/drawer.js
+++ js/drawer.js
@@ -4,14 +4,7 @@ var h          = 0.1;
 var iter       = 20;
 var zoom       = 2;
 var degree     = 2;
-//var div_iter   = createP('a');
-//var div_zoom   = createP('b');
-//var div_center = createElement('c'); 
-//var sizeSlider;
-//var iterSlider;
-//div_iter.html(iter);
-//div_zoom.html(zoom);
-//div_center.html(x_center, y_center);
+
 function mult([a,b],[c,d]) {
     return [a *c - b * d, a * d + b * c]
 }
@@ -26,72 +19,62 @@ function m(n, a, b) {
 }
 
 window.addEventListener('keydown', (event) => {
-    //console.log(event.key);
+
     if (event.key == 'ArrowRight') {
         x_center = x_center + h*zoom;
         document.getElementById("center").innerHTML = "Center coordinates: ".concat('(', x_center, ', ', y_center, ')');
-        //div_center.html(x_center, y_center);
-        //document.getElementById('center').innerText = toString(x_center, y_center);
     }
+
     if (event.key == 'ArrowLeft') {
         x_center = x_center - h*zoom;
         document.getElementById("center").innerHTML = "Center coordinates: ".concat('(', x_center, ', ', y_center, ')');
-        //div_center.html(x_center, y_center);
-        //document.getElementById('center').innerText = toString(x_center, y_center);
     }
+
     if (event.key == 'ArrowUp') {
         y_center = y_center - h*zoom;
         document.getElementById("center").innerHTML = "Center coordinates: ".concat('(', x_center, ', ', y_center, ')');
-        //div_center.html(x_center, y_center);
-        //document.getElementById('center').innerHTML = toString(x_center, y_center);
     }
+
     if (event.key == 'ArrowDown') {
         y_center = y_center + h*zoom;
         document.getElementById("center").innerHTML = "Center coordinates: ".concat('(', x_center, ', ', y_center, ')');
-        //div_center.html(x_center, y_center);
-        //document.getElementById('center').innerHTML = toString(x_center, y_center);
     }
+
     if (event.key == ' ') {
         iter = floor(iter * 2) ;
         document.getElementById("iter").innerHTML = "Number of iterations: ".concat(iter);
-        //document.getElementById('iter').innerHTML = toString(iter);
     }
+
     if (event.key == 'b' && iter > 1) {
         iter = floor(iter * 0.5) ;
         document.getElementById("iter").innerHTML = "Number of iterations: ".concat(iter);
-        //document.getElementById('iter').innerHTML = toString(iter);
     }
+
     if (event.key == 'q') {
         zoom = zoom * 0.9 ;
         document.getElementById("zoom").innerHTML = "Zoom: ".concat(zoom);
-        //document.getElementById('zoom').innerHTML = toString(zoom);
     }
+
     if (event.key == 'w') {
         zoom = zoom * 10/9;
         document.getElementById("zoom").innerHTML = "Zoom: ".concat(zoom);
-        //document.getElementById('zoom').innerHTML = toString(zoom);
     }
+
     if (event.key == 'o') {
         degree ++;
         document.getElementById("degree").innerHTML = "".concat('z^',degree, ' + c');
     }
+
     if (event.key == 'p' && degree > 1) {
         degree --;
         document.getElementById("degree").innerHTML = "".concat('z^',degree, ' + c');
-    }
-    
+    }    
 })
 
-//var frDiv;
 function setup() {
     var canvas = createCanvas(360, 360);
     canvas.parent('canvasHere')
     pixelDensity(1);
-
-    //ySlider    = createSlider(-2, 2, 0, 0.0001);
-    //xSlider    = createSlider(-2, 2, 0, 0.0001);
-    //sizeSlider = createSlider(0.00001, 4, 4 , 0.000001);
-    //iterSlider = createSlider(1, 500, 25, 1);
 }
 
 function draw() {
@@ -148,8 +131,4 @@ loadPixels();
         }
     }
     updatePixels();
-    //document.getElementById('center').innerText = toString(x_center, y_center);
-    //document.getElementById(elementID).innerHTML = "";
-    //document.getElementById('frDiv').innerHTML = toString(floor(frameRate()));
-    //frDiv.html(floor(frameRate()));
 }
